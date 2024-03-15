737fd81bb28b42810a0d1a04b19d4d4f96167239
voisinage <154838445+voisinage@users.noreply.github.com>
2024-03-15 00:05:31 -0400

    browser

--- src/rpart/z101_PrimerModelo.R
+++ src/rpart/z101_PrimerModelo.R
@@ -24,7 +24,7 @@ modelo <- rpart(
         cp = -0.3, # esto significa no limitar la complejidad de los splits
         minsplit = 0, # minima cantidad de registros para que se haga el split
         minbucket = 1, # tamaño minimo de una hoja
-        maxdepth = 3
+        maxdepth = 10
 ) # profundidad maxima del arbol
 
 
